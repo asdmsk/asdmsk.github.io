123d083eb8b5401377fc1da74b17ab36651fbaef
asdmsk <asdmsk1@gmail.com>
2026-03-02 22:48:43 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'akd-final-v9';
+const CACHE_NAME = 'akd-final-v10';
 const GH_PATH = ''; // Это критично: теперь путь пустой, так как мы в корне домена
 
 const ASSETS_TO_CACHE = [
